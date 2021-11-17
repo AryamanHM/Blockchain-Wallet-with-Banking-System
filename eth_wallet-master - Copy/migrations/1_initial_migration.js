cdbcf2f8259b707b2af2ae929f3af3e56859ed41
Aryaman Mishra <aryaman.mishra73@gmail.com>
2021-11-17 15:28:59 +0530

    Added final files

---
+++ eth_wallet-master - Copy/migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xbee591A8FD2CB68ba569Ea043b25FF7E0E5f88A9',
+    '1000000000000000000000'
+  )
+};
